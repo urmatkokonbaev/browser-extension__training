b918edaf8b7d906aaf87b83933bb130345dbc3ce
Urmat Kokonbaev <urmat.kokonbaev@pearson.com>
2024-07-24 01:16:21 +0600

    function to copy text in clipboard

--- src/content-script.js
+++ src/content-script.js
@@ -1,1 +1,20 @@
-console.log('hello from content script');
+const preEls = document.querySelectorAll('pre');
+
+const buttonKeys = Object.freeze({
+    innerText: 'Copy',
+    type: 'button' 
+});
+
+[...preEls].forEach(preEl => {
+    const button = document.createElement('button')
+    button.innerText = buttonKeys.innerText
+    button.type = buttonKeys.type
+
+    preEl.append(button)
+
+    const codeEl = preEl.querySelector('code')
+
+    button.addEventListener('click', () => {
+        navigator.clipboard.writeText(codeEl.innerText)
+    })
+})
